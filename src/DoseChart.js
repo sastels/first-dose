a80d568d39fb9723f8bd4f858d9b22d25d3d54d5
Steve Astels <sastels@gmail.com>
2021-05-19 11:51:43 -0400

    rough in recharts

--- src/DoseChart.js
+++ src/DoseChart.js
@@ -1,6 +1,46 @@
 
 import React,{useState,useEffect} from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+function Chart(data) {
 
+
+    const canada = data["Canada"] || "no data"
+
+    var munged = []
+    for (const key in canada) {
+        munged.push({date: key, doses: canada[key]})
+    }
+
+    const d = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+    {name: 'Page B', uv: 500, pv: 2400, amt: 2400},
+    {name: 'Page C', uv: 200, pv: 2400, amt: 2400},
+    {name: 'Page D', uv: 600, pv: 2400, amt: 2400},
+]
+    
+    return (
+
+        <div>
+        {JSON.stringify(canada)}
+        
+        <LineChart width={600} height={300} data={munged}>
+            <Line type="monotone" dataKey="doses" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="date" />
+            <YAxis />
+        </LineChart>
+
+
+        <LineChart width={600} height={300} data={d}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            {/* <XAxis dataKey="name" /> */}
+            <YAxis />
+        </LineChart>
+
+        </div>
+
+    )
+}
 
 function DoseChart() {
     const [data,setData]=useState([]);
@@ -21,7 +61,6 @@ function DoseChart() {
               for (const key in data) {
                 console.log(`${key}: ${JSON.stringify(data[key])}`);
               }
-
             setData(data);
           });
       }
@@ -34,7 +73,7 @@ function DoseChart() {
       return (
         <div className="App">
          {
-           data && "data"
+           Chart(data)
          }
         </div>
       );
