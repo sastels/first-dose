1e635ac55f51b8c5c170031cfbca25afc871f07d
Steve Astels <sastels@users.noreply.github.com>
2021-05-19 16:44:09 -0400

    Deploy (#4)

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import DoseChart from './DoseChart';
 function App() {
   return (
     <div style={{textAlign:"center"}}>
-    <div style = {{margin : 50 }} className="App">
-      <DoseChart/>
-    </div>
-    <p>
-      Percentage of a country that has been at least one dose of a Covid-19 vaccine. 
-      Data from <a href="https://github.com/owid/covid-19-data">Our World in Data</a>.
-    </p>
+      <div style = {{margin : 50 }} className="App">
+        <DoseChart/>
+      </div>
+      <p>Percentage of people in a country who has had at least one dose of a Covid-19 vaccine.</p>
+      <p>Data from <a href="https://github.com/owid/covid-19-data">Our World in Data</a>.</p>
     </div>
   );
 }
