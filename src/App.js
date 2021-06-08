3dff2fb8c349b7e3364dde0851852c31366c9079
Steve Astels <sastels@users.noreply.github.com>
2021-06-08 14:46:52 -0400

    tweak updated date (#11)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import DoseChart from './DoseChart';
-
+import "./App.css";
+import DoseChart from "./DoseChart";
 
 function App() {
   return (
-    <div style={{textAlign:"center"}}>
-      <div style = {{margin : 50 }} className="App">
-        <DoseChart/>
+    <div style={{ textAlign: "center" }}>
+      <div style={{ margin: 50 }} className="App">
+        <DoseChart />
       </div>
-      <p>Percentage of people in a country who have had at least one dose of a Covid-19 vaccine.</p>
-      <p>Data from <a href="https://github.com/owid/covid-19-data">Our World in Data</a>.</p>
-      <p>Code in <a href="https://github.com/sastels/first-dose">Github</a>.</p>
+      <p>
+        Data from{" "}
+        <a href="https://github.com/owid/covid-19-data">Our World in Data</a>.
+      </p>
+      <p>
+        Code in <a href="https://github.com/sastels/first-dose">Github</a>.
+      </p>
     </div>
   );
 }
