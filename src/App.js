ad29c448941aaeeb47c556762f81f11111d85474
Steve Astels <sastels@gmail.com>
2021-06-10 18:56:26 -0400

    attribution

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ function App() {
       </div>
       <p>
         Data from{" "}
-        <a href="https://github.com/owid/covid-19-data">Our World in Data</a>
+        <a href="https://github.com/owid/covid-19-data">Our World in Data</a>,{" "}
+        <a href="https://api.covid19tracker.ca">COVID-19 Tracker Canada</a>,
         {" and "}
-        <a href="https://api.covid19tracker.ca">COVID-19 Tracker Canada</a>.
+        <a href="https://open.ottawa.ca/">Open Ottawa</a>.
       </p>
       <p>
         Code in <a href="https://github.com/sastels/first-dose">Github</a>.
